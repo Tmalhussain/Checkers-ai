3e13f09fd3aa175fab54425c10b38a0a6c5b6146
Tmalhussain <93242311+Tmalhussain@users.noreply.github.com>
2023-11-18 12:51:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import pygame as pg
 from checkers.constants import *
 from checkers.game import *
 from minimax.algorithm import minmax
-
 FPS = 60
 
 WIN = pg.display.set_mode((WIDTH, HEIGHT))
@@ -15,14 +14,37 @@ def get_row_col_from_mouse(pos):
     col = x // SQUARE_SIZE
     return row, col
 
-def main():
+def menu():
+    pg.font.init()
+    clock = pg.time.Clock()
+    run = True
+    while run:
+        WIN.fill((0,0,0))
+        font = pg.font.Font(None, 50)
+        text = font.render("Press 1 for AI mode, 2 for two-player mode", 1, (255, 255, 255))
+        WIN.blit(text, (WIDTH // 2 - text.get_width() // 2, HEIGHT // 2 - text.get_height() // 2))
+        pg.display.update()
+
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                run = False
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_1:
+                    main(ai_mode=True)
+                elif event.key == pg.K_2:
+                    main(ai_mode=False)
+
+        clock.tick(FPS)
+
+def main(ai_mode):
+    pg.font.init()
     run = True
     clock = pg.time.Clock()
     game = Game(WIN)
 
     while run:
         clock.tick(FPS)
-        if game.turn == WHITE:
+        if game.turn == WHITE and ai_mode:
             value, new_board = minmax(game.get_board(), 4, WHITE, game)
             game.ai_move(new_board)
         if game.winner():
@@ -39,7 +61,4 @@ def main():
                 game.select(row, col)
 
         game.update()
-    
-    pg.quit()
-
-main()
+menu()
